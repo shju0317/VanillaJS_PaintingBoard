942d74c3e55a8f92b29a54b6b5a81592333ab12c
shju0317 <shju0317@naver.com>
2020-10-07 21:06:07 +0900

    fill

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
-canvas.width = 450;
-canvas.height = 450;
+const INITIAL_COLOR = "2c2c2c";
+const CANVAS_SIZE = 450;
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
   painting = false;
@@ -33,6 +44,12 @@ function onMouseMove(event) {
   //console.log(x, y);
 }
 
+function handleRangeChange(event) {
+  //console.log(event.target.value);
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
 function onMouseDown(event) {
   startPainting();
 }
@@ -49,6 +66,36 @@ function handleColorClick(event) {
   console.log(event.target.style);
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+}
+
+function handleModeClick(event) {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
+}
+
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
+}
+
+function handleCM(event) {
+  event.preventDefault();
+}
+
+function handleSaveClick() {
+  const image = canvas.toDataURL();
+  const link = document.createElement("a");
+
+  link.href = image;
+  link.download = "PaintJS[EXPORT]";
+  link.click();
 }
 
 if (canvas) {
@@ -56,7 +103,8 @@ if (canvas) {
   canvas.addEventListener("mousedown", onMouseDown);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", onMouseLeave);
-  canvas.addEventListener;
+  canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleCM);
 }
 
 console.log(Array.from(colors));
@@ -64,3 +112,15 @@ console.log(Array.from(colors));
 Array.from(colors).forEach((color) =>
   color.addEventListener("click", handleColorClick)
 );
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick);
+}
